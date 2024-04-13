6588149d3191b32f4d4c7d69fa29c1c6fcdb8c0f
Seungpyo1007 <rush94434@gmail.com>
2024-04-14 02:01:32 +0900

    Base

--- module/src/main/cpp/include/config.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace Module {
-    extern const int moduleVersionCode;
-    extern const char *const moduleVersionName;
-}
--- module/src/main/cpp/template/config.cpp
+++
@@ -1,6 +0,0 @@
-#include "config.hpp"
-
-namespace Module {
-    const int moduleVersionCode = ${MODULE_VERSION};
-    const char *const moduleVersionName = "${MODULE_VERSION_NAME}";
-}
